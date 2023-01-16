e7dbf26db56bdddaa708dca959531f0a09e59f29
Aline Guiseline <alineguiseline@gmail.com>
2023-01-16 17:01:29 -0300

    Criação do HTML e do CSS

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
-  // ...
-};
+  
+}
 
-export default validator;
+/* const validateButton = document.getElementById("btn-validate");
+const cardNum = document.getElementById("cardumber"); */
+
+const meuArray = [1, 2, 5, 7, 9, 4];
+const meuArrayInvertido = meuArray.slice(0).reverse();
+
+
+if (meuArrayInvertido.length % 2 === 0) {
+    
+} else {
+    
+}
+
+// export default validator;
