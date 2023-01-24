9f6161b8963be367698768573c8ad88aaa56f4f9
Aline Guiseline <alineguiseline@gmail.com>
2023-01-24 14:49:39 -0300

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,70 @@
 import validator from './validator.js';
 
+/* O que é, genuinamente, necessário:
+1- Pegar o botão
+2- Pegar o valor do input (número do cartão)
+3- Ligar essas duas coisas com a lógica do validator.js
+   O que eu quero tentar (mas, se não der, vou usar um alert):
+Linkar as mensagens de válido/inválido ao parágrafo vazio */
+
+const btnCard = document.getElementById("btnValidate"); //Vai pegar o botão
+const alertCard = document.getElementById("cardResult"); //Vai pegar o parágrafo vazio abaixo do botão
+
+btnCard.addEventListener('click', cardValidator);
+
+function cardValidator() {
+  const creditCardNumber = document.getElementById("number").value; //Vai pegar o valor do input do número do cartão
+  const finalResult = validator.isValid(creditCardNumber); //Vai linkar com a "fórmula" usada no validator.js
+  const maskedNumbers = validator.maskify(creditCardNumber) //Também vai linkar com a fórmula usada no validator.js
+
+  if (creditCardNumber === "") {
+    return alertCard.innerText = "Por favor, digite o número do seu cartão."; //O .innerText vai inserir um texto ao parágrafo vazio
+  } else if (creditCardNumber.length <= 13) {
+    return alertCard.innerText = "Ops. Faltam alguns dígitos no número do seu cartão. Favor verificar.";
+  } else if (finalResult === true) {
+    return alertCard.innerText = "Oba!! O seu cartão de número " + maskedNumbers + " está válido e nós prosseguiremos com a sua compra ❤";
+  } else {
+    return alertCard.innerText = "Infelizmente, o seu cartão de número " + maskedNumbers + " não está válido. Favor verificar.";
+  }
+}
+
+const cardNumber = document.getElementById("number")
+const cardName = document.getElementById("name")
+const cardExpiry = document.getElementById("expiry")
+const cardCVV = document.getElementById("cvv")
+
+cardNumber.addEventListener("keyup", cloneCardNumber);
+cardName.addEventListener("keyup", cloneCardName);
+cardExpiry.addEventListener("keyup", cloneCardExpiry);
+cardCVV.addEventListener("keyup", cloneCardCVV);
+
+
+function cloneCardNumber() {
+  const value = document.getElementById("number").value;
+  const mask = validator.maskify(value);
+
+  document.getElementById("card-number").value = mask;
+
+  document.getElementById("card-date").value
+  document.getElementById("card-cvv").value
+}
+
+function cloneCardName() {
+  const value = document.getElementById("name").value
+
+  document.getElementById("card-name").value = value
+}
+
+function cloneCardExpiry() {
+  const value = document.getElementById("expiry").value
+
+  document.getElementById("card-expiry").value = value
+}
+
+function cloneCardCVV() {
+  const value = document.getElementById("cvv").value
+
+  document.getElementById("card-cvv").value = value
+}
+
 console.log(validator);
--- validator.js
+++ validator.js
@@ -1,5 +1,61 @@
 const validator = {
-  // ...
-};
+
+  //      1- Receber os dados do usuário
+  isValid (creditCardNumber) {
+    const newArray = []; //Cria um array vazio para receber os dados que o usuário colocar
+    for(let i = 0; i < creditCardNumber.length; i++) {
+      const numCard = parseInt(creditCardNumber[i]); //O parseInt analisa a string e retorna um número, o que evita erros de concatenação
+      newArray.push(numCard) //O .push adiciona novos elementos ao array; ou seja, os números que serão recebidos no input serão alinhados aqui
+    }
+
+    //     2- Inverter a ordem dos números
+
+    const cardNumReverse = newArray.reverse(); //Vamos criar uma nova variável para receber a string, que será revertida pelo .reverse()
+
+    //      3- Encontrar os números que estão nas posições pares
+    //      4- Somar esses números
+    //       4.1- Se a soma der um número maior do que 9, pegar esse resultado e subtrair 9
+
+    for (let i = 0; i < cardNumReverse.length; i++) {
+      if ((i + 1) % 2 === 0) { //Esse i+1 vai dizer ao JS que ele precisa começar do segundo elemento (se colocarmos só i % 2 === 0, ele vai começar do primeiro)
+        cardNumReverse[i] = cardNumReverse[i] * 2; //Agora ele vai pegar cada posição par (indicada pelo [i]) e vai multiplicar por 2
+      }
+      if (cardNumReverse[i] >= 10) {
+        cardNumReverse[i] = cardNumReverse[i] - 9; //Se o número for maior ou igual a 10, ele vai subtrair 9
+      }
+    }
+      
+    //      5- Somar todos os números
+    //      5.1- Verificar se o resultado é um múltiplo de 10; se for, o cartão é válido e se não, o cartão é inválido
+      
+    let finalSum = 0
+    for (let i = 0; i < cardNumReverse.length; i++) { //Vamos reutilizar a const cardNumReverse que está "guardando" o array invertido
+      finalSum = finalSum + cardNumReverse[i]
+    }
+      
+    if (finalSum % 10 === 0) {
+      return true
+    } else {
+      return false
+    }
+  },
+
+  /* maskify (creditCardNumber){
+    return creditCardNumber.replace(/.(?=.{4})/g, "#") //Pesquisando sobre como mascarar os dígitos encontrei o conceito de RegEx (esse já estava pronto), que são padrões usados para selecionar combinações de caracteres em uma string
+  }*/
+
+  maskify (creditCardNumber){
+    const maskNumber = []; //Array vazio para receber os dados que não temos de antemão (usuário que vai informar)
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if(i < creditCardNumber.length - 4) { //Isso vai "isolar" os 4 últimos números (testei com === e > e não funcionou), que são os que nós precisamos
+        maskNumber.push("#"); //Vai substituir os números por # até chegar aos 4 dígitos finais
+      } else {
+        maskNumber.push(creditCardNumber[i]) //Vai manter os números normais
+      }
+    }
+    const masked = maskNumber.join("");
+    return masked;
+  }
+}
 
 export default validator;
