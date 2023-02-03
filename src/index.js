4caf162350acb5f410415cbcbef644cf5974b4b9
AlineGuiseline <alineguiseline@gmail.com>
2023-02-03 16:48:45 -0300

    Alterações no js e no css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import validator from './validator.js';
 
-/* O que é, genuinamente, necessário:
-1- Pegar o botão
-2- Pegar o valor do input (número do cartão)
-3- Ligar essas duas coisas com a lógica do validator.js
-   O que eu quero tentar (mas, se não der, vou usar um alert):
-Linkar as mensagens de válido/inválido ao parágrafo vazio */
-
 const btnCard = document.getElementById("btnValidate"); //Vai pegar o botão
 const alertCard = document.getElementById("cardResult"); //Vai pegar o parágrafo vazio abaixo do botão
 
